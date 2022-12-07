93fc563d6255693e2042bf4283bb1c95ff0c1571
thanhnd1503 <thanhnd15032002@gmail.com>
2022-12-07 15:35:06 +0700

    Add js file

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+function showMessage(){
+    alert('Xin chào!');
+}
+function showMess2() {
+    document.getElementById("messdiv").innerHTML="Xin chao !";
+}
+function showMess3() {
+    document.write('Xin chào!');
+}
+function showMess4(){
+    console.log('Xin chao!');
+
+}
